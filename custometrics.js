3a3dcbb19fb80f018e16bc20ddd72b286a721853
Sreelakshminarayanan Muthuraj <sreelakshminarayanan@gmail.com>
2024-02-28 20:16:41 +0530

    update read me file

---
+++ custometrics.js
@@ -0,0 +1,30 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+import { Counter, Gauge, Trend, Rate} from 'k6/metrics';
+
+const http403StatusCounter = new Counter("http403StatusCounter");
+const http200StatusCounter = new Counter("http200StatusCounter");
+const customTrend = new Trend("custom_duration");
+const customGauge = new Gauge("custom_gauge");
+const customRate = new Rate("custom_rate");
+export const options = {
+  // A number specifying the number of VUs to run concurrently.
+  vus: 10,
+  // A string specifying the total duration of the test run.
+  duration: '1s',
+};
+
+
+export default function() {
+  const res = http.get('https://test.k6.io');
+  check(res, {
+    'is status 200':  r => r.status ==200
+  });
+  customTrend.add(res.timings.duration)
+  if (res.status === 200 ) {
+    http200StatusCounter.add(1);
+  } else if ( res.status === 403) {
+    http403StatusCounter.add(1);
+  }
+  sleep(1);
+}
---
+++ executor.js
@@ -0,0 +1,35 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+export const options = {
+  discardResponseBodies: true,
+  scenarios: {
+    orders: {
+        executor: 'ramping-vus',
+        startVUs: 0,
+        exec: 'checkOrders',
+        stages: [
+          { duration: '20s', target: 10 },
+          { duration: '10s', target: 0 },
+        ],
+        gracefulRampDown: '0s',
+    },
+    review: {
+      executor: 'constant-vus',
+      exec: 'checkReviews',
+      vus: 5,
+      duration: '30s',
+    },
+  },
+};
+
+export function checkOrders() {
+  http.get('https://test.k6.io/contacts.php', {
+    tags: { my_custom_tag: 'orders' },
+  });
+}
+
+export function checkReviews() {
+  http.get('https://test.k6.io/news.php', { tags: { my_custom_tag: 'review' } });
+  sleep(1)
+}
